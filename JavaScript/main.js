3a61d58529b041dc038b44172a6c0d893cbe8745
kushal atreya <kushalatreya@live.com>
2018-08-29 09:32:15 +0300

    ScreenShot

---
+++ JavaScript/main.js
@@ -0,0 +1,12 @@
+console.log("Hello World!");
+var birthYear = 1995;
+var atomicRadius = 1.5e-5;
+var num1 = 3;
+var num2 = 100;
+var sum = num1 + num2;
+var diff = num2 - num1;
+var mult = num1 * num2;
+var div = num2 / num1;
+var rem = num2 % num1;
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+console.log(`The div of ${num1} and ${num2} is ${div.toFixed(3)}`);
