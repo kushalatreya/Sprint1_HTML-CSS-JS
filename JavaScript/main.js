71e2acdf0d5e9543cb18cc99a99926aa1a7942b4
kushal atreya <kushalatreya@live.com>
2018-09-01 20:50:51 +0300

    JavaScript file Added

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,26 +3,8 @@
 
 //The assignment starts from here:
 
-/* Exercise: 1
-Part I
-Declare a variable name company and assign it to an initial value “Integrify Academy”.
-Print the string  on the browser console using console.log()
-Print the length of the string  on the browser console using console.log()
-Change all the string to capital letters using toUpperCase() method
-Change all the string to small letters using toLowerCase() method
-Cut(slice) out the first word of the string using slice, substr() or substring() method
-Check if the string contains a word Academy using includes() method
-Split the string into array using split() method
-Change Integrify Academy  to Microsoft Academy using replace() method.
-What is the index of A in ‘Integrify Academy’ string use charAt()
-What is the character code of A in ‘Integrify Academy’ string using charCodeAt()
-Use indexOf to determine the position of  the first occurrence of e in Integrify Academy
-Use lastIndexOf to determine the position of the last occurrence of e in Integrify Academy
-Use trim() to remove if there is trailing whitespace at the beginning and the end of the a string.E.g “  Integrify Academy   ”
-Use startsWith() method with the string Integrify Academy make the result true
-Use endsWith() method with the string Integrify Academy make the result true
- */
 
+//Q1.
 /* var company = "Integrify Academy";
 console.log(company);
 console.log(company.length);
@@ -101,24 +83,12 @@ You are 5 years older than me.
         document.write(` </br> You are ${yourAge - myAge} years older than me.`);
         }
 } */
-/* 5. Declare an array  variable name itCompanies and assign initial values Facebook, Google, Microsoft, IBM, Apple, Oracle and Amazon.
-Print the array using console.log()
-Print the number of companies in the array
-Print out each company
-Capitalize each company and print them out ***
-Print the array like this: Facebook, Google, Microsoft, Apple, Oracle and Amazon are big IT companies.
-Sort the array using sort() method
-Reverse the array using reverse() method */
 
 var itCompanies = ["Facebook", "Google", "Microsoft", "IBM", "Apple", "Oracle", "Amazon"];
 console.log(itCompanies);
 console.log(itCompanies.length + " companies");
 console.log(itCompanies.toString().toUpperCase());
-
 console.log(itCompanies.slice(0, 6).join(" , ") + " are big companies ")
-
-
-
 console.log(itCompanies.sort());
 console.log(itCompanies.reverse()); //reverse
 
@@ -129,11 +99,8 @@ for loop and print out the items. */
 /* 7. Declare a function name printArray.It takes array as a parameter and it prints out. 
  */
 
-
-
 function printArray(array) {
     console.log(array);
-
 };
 
 /* 8. Declare a function name reverseArray.It takes array as a parameter and it returns the reverse of the array
